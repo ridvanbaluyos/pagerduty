b55fc9be3513b236424b730b7a5d2c54d6b04e78
Ridvan Baluyos <ridvan@baluyos.net>
2016-03-23 17:47:20 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+ridvanbaluyos/pagerduty
+=======
+A PHP Library built for the [PagerDuty API](https://developer.pagerduty.com/).
+<br/><br/>
---
+++ src/Ridvanbaluyos/Pagerduty/PagerDuty.php
@@ -0,0 +1,14 @@
+<?php 
+namespace Ridvanbaluyos\Pagerduty;
+/**
+ * FaceDetection
+ *
+ * @package    Pagerduty
+ * @author     Ridvan Baluyos <ridvan@baluyos.net>
+ * @link       https://github.com/ridvanbaluyos/pagerduty
+ * @license    MIT
+ */
+class Pagerduty
+{
+	
+}
